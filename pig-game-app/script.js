29c316d7498630a774b143b3ea01a5e0644f8752
Martyna Markiel <martyna.markiel@gmail.com>
2021-08-13 17:32:49 +0200

    comments refactor

--- pig-game-app/script.js
+++ pig-game-app/script.js
@@ -1,27 +1,26 @@
 'use strict';
 
-// Selecting elements
+// selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnNHold = document.querySelector('.btn--hold');
 
-// declaring the variables so they live outside the function and are accessible everywhere
+// declaring the variables so they live outside the init f and are accessible everywhere
 let scores, currentScore, activePlayer, playing;
 
-// Starting conditions
+// starting conditions
 const init = function () {
   scores = [0, 0];
   currentScore = 0;
   activePlayer = 0;
-  playing = true; // state variable of the game
+  playing = true; // state of the game
 
   score0El.textContent = 0;
   score1El.textContent = 0;
@@ -45,7 +44,7 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-// Rolling dice functionality
+// rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. generating a random dice roll
@@ -56,9 +55,9 @@ btnRoll.addEventListener('click', function () {
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
-    // 3. Check for rolled 1
+    // 3. check for rolled 1
     if (dice !== 1) {
-      // Add dice to current score
+      // add dice to current score
       currentScore += dice;
       document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
